3b0c8a39f070d947e219437f4d89bf9c30d77389
Im60duck <kvashnin.andruha@yandex.ru>
2024-11-23 04:17:16 +0700

    stuffy linter demands

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,16 @@
-lengthCheck = function(str, size){
-  return (str.length <= parseInt(size));
-},
-isPalindrom = function(str){
+var lengthCheck = function(str, size){
+  return str.length <= parseInt(size, 10);
+}
+var isPalindrom = function(str){
   return(str.toLowerCase() === str.toLowerCase().split('').reverse().join(''));
-},
-findDigits = function(str){
+}
+var findDigits = function(str){
   const r = /\d+/g;
   let result = '';
-  str.toString().split('').forEach(element => {
-    if(element.match(r) != null){
+  str.toString().split('').forEach((element) => {
+    if(element.match(r) !== null){
       result += element;
-    };
+    }
   });
-  return (result === '') ? NaN : parseInt(result);
+  return (result === '') ? NaN : parseInt(result, 10);
 }
