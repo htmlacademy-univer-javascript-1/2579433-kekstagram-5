b95c7d16c9e6b5b339f5c4b4f2ce5abdb29f89e9
Im60duck <kvashnin.andruha@yandex.ru>
2024-11-23 04:21:15 +0700

    stuffy linter demands#2

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,10 @@
-var lengthCheck = function(str, size){
+const lengthCheck = function(str, size){
   return str.length <= parseInt(size, 10);
-}
-var isPalindrom = function(str){
+};
+const isPalindrom = function(str){
   return(str.toLowerCase() === str.toLowerCase().split('').reverse().join(''));
-}
-var findDigits = function(str){
+};
+const findDigits = function(str){
   const r = /\d+/g;
   let result = '';
   str.toString().split('').forEach((element) => {
@@ -13,4 +13,4 @@ var findDigits = function(str){
     }
   });
   return (result === '') ? NaN : parseInt(result, 10);
-}
+};
