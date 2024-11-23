6b5f2debed9393ef29fd450d8611f8f95e23dd34
Keks <keks@htmlacademy.ru>
2024-11-23 08:14:41 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/functions.js
+++ 3/js/functions.js
@@ -1,17 +1,16 @@
-(lengthCheck = function(str, size){
-  return (str.length <= parseInt(size));
-},
-isPalindrom = function(str){
-  return(str === str.split('').reverse().join(''));
-},
-findDigits = function(str){
+const lengthCheck = function(str, size){
+  return str.length <= parseInt(size, 10);
+};
+const isPalindrom = function(str){
+  return(str.toLowerCase() === str.toLowerCase().split('').reverse().join(''));
+};
+const findDigits = function(str){
   const r = /\d+/g;
   let result = '';
-  str.split('').forEach(element => {
-    if(element.match(r) != null){
+  str.toString().split('').forEach((element) => {
+    if(element.match(r) !== null){
       result += element;
-    };
+    }
   });
-  return result;
-}
-)
+  return (result === '') ? NaN : parseInt(result, 10);
+};
