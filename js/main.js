50538727217607c088ab202c832cf761fbf706a8
Im60duck <kvashnin.andruha@yandex.ru>
2024-11-27 20:35:53 +0700

    Linter correction

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 const uid = function(){
-  return Date.now() % 1000000000;
-}
-
-const createDescription = function(){}
+  return Date.now();
+};
 
 const createComment = function(){
   const messages = [
@@ -16,13 +14,13 @@ const createComment = function(){
 
   const names = ["Аамон", "Амон", "Амун", "Аман", "Аманд", "Абигор", "Элигору", "Элигос", "Эрртруар"];
 
-  return comment = {
+  return {
     id: uid(),
     avatar: `img/avatar-${Math.floor((Math.random() * 10) % 7)}.svg`,
     message: messages[Math.floor((Math.random() * 10) % 6)] + messages[Math.floor((Math.random() * 10) % 6)],
     name: names[Math.floor((Math.random() * 100) % names.length)],
-  }
-}
+  };
+};
 
 const generateComments = function(){
   const max = Math.floor((Math.random() * 100) % 31);
@@ -31,23 +29,23 @@ const generateComments = function(){
     comments.push(createComment());
   }
   return comments;
-}
+};
 
 const createPost = function(id, url){
   const descriptions = ["Hope eradicated", "Meat automaton", "Divine light", "Power in misery", "CEO mindset", "Funny pills"];
-  return post = {
+  return {
     id: id,
     url: `photos/${url}.jpg`,
     description: descriptions[Math.floor((Math.random() * 10) % descriptions.length)],
     likes: Math.floor((Math.random() * 1000) % (200 - 15) + 15),
     comments: generateComments(),
-  }
-}
+  };
+};
 
 const constructTask = function(){
   for(let i = 1; i <= 25; i++){
-    const post = createComment(i, i);
+    const post = createPost(i, i);
     console.log(post);
     console.log(post.comments);
   }
-}
+};
