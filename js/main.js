cedb83ef6b8da4b23e8e348b584c6f1f34ec5e32
Im60duck <kvashnin.andruha@yandex.ru>
2024-11-27 20:07:10 +0700

    Temporary data generation

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const uid = function(){
+  return Date.now() % 1000000000;
+}
+
+const createDescription = function(){}
+
+const createComment = function(){
+  const messages = [
+    "Всё отлично!",
+    "В целом всё неплохо. Но не всё.",
+    "Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.",
+    "Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.",
+    "Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.",
+    "Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!"
+  ];
+
+  const names = ["Аамон", "Амон", "Амун", "Аман", "Аманд", "Абигор", "Элигору", "Элигос", "Эрртруар"];
+
+  return comment = {
+    id: uid(),
+    avatar: `img/avatar-${Math.floor((Math.random() * 10) % 7)}.svg`,
+    message: messages[Math.floor((Math.random() * 10) % 6)] + messages[Math.floor((Math.random() * 10) % 6)],
+    name: names[Math.floor((Math.random() * 100) % names.length)],
+  }
+}
+
+const generateComments = function(){
+  const max = Math.floor((Math.random() * 100) % 31);
+  const comments = [];
+  for(let i = 0; i < max; i++){
+    comments.push(createComment());
+  }
+  return comments;
+}
+
+const createPost = function(id, url){
+  const descriptions = ["Hope eradicated", "Meat automaton", "Divine light", "Power in misery", "CEO mindset", "Funny pills"];
+  return post = {
+    id: id,
+    url: `photos/${url}.jpg`,
+    description: descriptions[Math.floor((Math.random() * 10) % descriptions.length)],
+    likes: Math.floor((Math.random() * 1000) % (200 - 15) + 15),
+    comments: generateComments(),
+  }
+}
+
+const constructTask = function(){
+  for(let i = 1; i <= 25; i++){
+    const post = createComment(i, i);
+    console.log(post);
+    console.log(post.comments);
+  }
+}
